39d682ed46550da6ad33a9f35e370f91a3738044
Nitin Gurram <nigurr@microsoft.com>
2017-01-04 17:07:37 +0530

    Initial commit

---
+++ Tasks/VsTest/constants.ts
@@ -0,0 +1,10 @@
+export class Constants {
+    public static runSettingsExt: string = '.runsettings';
+    public static testSettingsExt: string = '.testsettings';
+    public static testImpactFriendlyName: string = 'Test Impact';
+    public static testImpactCollectorURI: string = 'datacollector://microsoft/TestImpact/1.0';
+    public static testImpactTestSettingsAgentNameTag: string = 'testImpact-5d76a195-1e43-4b90-a6ce-4ec3de87ed25';
+    public static testImpactTestSettingsNameTag: string = 'testSettings-5d76a195-1e43-4b90-a6ce-4ec3de87ed25';
+    public static testImpactTestSettingsIDTag: string = '5d76a195-1e43-4b90-a6ce-4ec3de87ed25';
+    public static testImpactTestSettingsXmlnsTag: string = 'http://microsoft.com/schemas/VisualStudio/TeamTest/2010'
+}
